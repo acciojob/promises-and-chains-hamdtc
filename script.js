23b2368f947553fcde2c30d9c31a51151ed658c5
Mohamed t c <97926192+hamdtc@users.noreply.github.com>
2023-03-15 07:17:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+let form = document.querySelector('form');
+let age = document.getElementById('age');
+let name = document.getElementById('name');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+  checkCanIVote(age.value).then((data) => {
+    alert("Welcome, "+name.value+" "+data);
+  }).catch((data)=>{
+    alert("Oh sorry "+name.value+". "+data);
+  })
+})
+
+
+
+function checkCanIVote(number) {
+    // return the output using return keyword
+    // do not console.log it
+	let prom = new Promise((resolve, reject) =>{
+		if(number >= 18){
+			resolve("You can vote.");
+		}else{
+		    reject("You aren't old enough.");
+		}
+	});
+
+	return prom;
+}
