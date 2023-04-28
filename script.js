0f656c21bf86f0c2eea66c49ef62262be36b4430
Mohamed t c <97926192+hamdtc@users.noreply.github.com>
2023-04-28 21:26:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
-//your JS code here. If required.
-let form = document.querySelector('form');
-let age = document.getElementById('age');
-let name = document.getElementById('name');
+<!DOCTYPE html>
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-  checkCanIVote(age.value).then((data) => {
-    alert("Welcome, "+name.value+" "+data);
-  }).catch((data)=>{
-    alert("Oh sorry "+name.value+". "+data);
-  })
-})
+<html>
+  <head>
+    <link rel="stylesheet" href="styles.css" />
+  </head>
 
+  <body>
+    <!-- write the body here -->
+<form id="myForm">
+	<label for="name">Name:</label>
+	<input type="text" id="name" name="name" required>
 
+	<label for="age">Age:</label>
+	<input type="number" id="age" name="age" required>
 
-function checkCanIVote(number) {
-    // return the output using return keyword
-    // do not console.log it
-	let prom = new Promise((resolve, reject) =>{
-		if(number >= 18){
-			resolve("You can vote.");
-		}else{
-		    reject("You aren't old enough.");
-		}
-	});
+	<button type="submit" id="btn">Submit</button>
+	
+    <script type="text/javascript" src="./script.js"></script>
+  </body>
+</html>
+                                                                                                                                                                                                                                                          //your JS code here. If required.
+const nameInput = document.getElementById('name');
+const ageInput = document.getElementById('age');
+const submitBtn = document.getElementById('btn');
 
-	return prom;
-}
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault(); 
+  const delayPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Number(ageInput.value) >= 18) {
+        resolve({ name: nameInput.value, age: Number(ageInput.value) });
+      } else {
+        reject({ name: nameInput.value });
+      }
+    }, 4000);
+  });
+
+  delayPromise
+    .then((data) => {
+      alert(`Welcome, ${data.name}. You can vote.`);
+    })
+    .catch((error) => {
+      alert(`Oh sorry ${error.name}. You aren't old enough.`);
+    });
+});
